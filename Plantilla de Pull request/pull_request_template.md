9544b694557eda67d906128f28438d609fcf86bc
ianga <iang.menpor@educa.jcyl.es>
2023-11-11 18:13:06 +0100

    Creados getter y setters. Añadido carpeta "Assets" y imagen de diagrama de clases Lucidchart. Se ha creado sencilla Interfaz en clase main. Se ha introducido la Plantilla de Pull request

---
+++ Plantilla de Pull request/pull_request_template.md
@@ -0,0 +1,16 @@
+## Description de la tarea
+
+<!-- Se ha pedido la digitalización de una empresa de venta de automoviles, su diagrama de clases y codificación. El objetivo de la practica es la de usar el Pull Request y trabajar con ramas. -->
+
+## ¿Cómo se ha implementado?
+
+<!-- He usado diagramas de clase utilizando LucidChart para crear la estructura de clases del programa.
+Se ha creado un proyecto en Intellij IDEA donde en la rama Master se ha creado la estructura de clases y en la Rama "getter y setter" se ha codificado dichas clases. Además de añadir la plantilla.-->
+
+## Keywords
+
+<!-- #Diagrama de clases #Master #Branch #Pull request #Merge #Supervisor #Plantilla #assets-->
+
+## Screenshots or Video
+
+<!-- Captura de pantalla de la consola -->
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+public static void main (String [] args){
+
+    Scanner sc= new Scanner(System.in);
+
+    //-Titulo:
+    System.out.println("<--------Automoviles SL------->");
+
+    //-Datos del coche;
+    System.out.print("Rellene los datos del coche: \n->Matricula: ");
+    String licenseplate=sc.nextLine();
+    System.out.print("->Marca: ");
+    String brand=sc.nextLine();
+    System.out.print("->Modelo: ");
+    String model=sc.nextLine();
+    System.out.print("->Color: ");
+    String color=sc.nextLine();
+    System.out.print("->Tipo de combustible: ");
+    String fuelType=sc.nextLine();
+    System.out.print("->Motor: ");
+    String engine=sc.nextLine();
+    System.out.print("->Caballos de potencia");
+    String cv=sc.nextLine();
+    System.out.print("Precio: ");
+    String price=sc.nextLine();
+
+
+    Car coche = new Car();
+    coche.setLicensePlate(licenseplate);
+    coche.setBrand(brand);
+    coche.setModel(model);
+    coche.setColor(color);
+    coche.setFuelType(fuelType);
+    coche.setEngine(engine);
+    coche.setCv(cv);
+
+    //-Datos de Cliente
+    System.out.print("Rellene los datos del cliente: \n->Nombre: ");
+    String name=sc.nextLine();
+    System.out.print("->Apellidos: ");
+    String surname=sc.nextLine();
+    System.out.print("DNI: ");
+    String dni=sc.nextLine();
+    System.out.print("->Dirección: ");
+    String address=sc.nextLine();
+
+    Client cliente= new Client();
+    cliente.setName(name);
+    cliente.setSurname(surname);
+    cliente.setDni(dni);
+    cliente.setAddress(address);
+
+    //-Compra
+    System.out.print("Rellene los detalles de la compra: \n->Código:");
+    String code=sc.nextLine();
+    System.out.print("->Fecha: ");
+    String date = sc.nextLine();
+    System.out.print("->Método de Pago: ");
+    String paymentMethod=sc.nextLine();
+
+    Purchase compra= new Purchase();
+    compra.setCode(code);
+    compra.setDate(date);
+    compra.setPrice(price);
+    compra.setPaymentMethod(paymentMethod);
+
+    //No se usar ArrayLis... TuT
+
+    //-Ticket
+     System.out.println("<---------------------------TICKET---------------------------------------------------------------------------------------------------------------------------------------------------------------->");
+     System.out.println("En "+ compra.getDate()+ " , reunidos.");
+     System.out.println("De una parte Automoviles SL, en calidad de Vendedor. \nY de otra parte D. "+ cliente.getName()+" "+cliente.getSurname()+" con Documento Nacional de Identidad, nº. "+ cliente.getDni()+" domiciliado en "+ cliente.getAddress()+ " , en calidad de Comprador.");
+     System.out.println("             MANIFIESTAN               ");
+     System.out.println("Que han convenido, como el presente documento lo lleva en efecto, formalizar la compra-venta de vehículo automóvil cuyas características son las siguientes: \n      >Matrícula: "+coche.getLicensePlate()+"\n      >Marca y Tipo: "+ coche.getBrand()+ " "+ coche.getModel()+"\n      >Motor y Caballos de potencia: "+ coche.getEngine()+" "+coche.getCv()+ "\n      >Tipo de Combustible: "+ coche.getFuelType() +"\n      >Valor de Venta: "+ compra.getPrice()+ " por "+ compra.getPaymentMethod()+".");
+     System.out.println("<------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->");
+}
 }
